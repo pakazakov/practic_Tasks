46592442f3e7e05488f78170119bcec62c9a5b81
pakazakov <pa.kazakov.yar@mail.ru>
2022-10-25 22:02:41 +0300

    Add file

---
+++ tasks_7/7.2.py
@@ -0,0 +1,4 @@
+string_1 = (set(str(input())))
+string_2 = (set(str(input())))
+print(string_1 - string_2)
+
---
+++ tasks_7/7.3.py
@@ -0,0 +1,3 @@
+string_1 = (set(str(input())))
+string_2 = (set(str(input())))
+print(string_1 & string_2)
